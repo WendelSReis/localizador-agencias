9155da58e69ba911948359619843144f424444ca
WendelSReis <wendel.m.w18@gmail.com>
2025-09-08 09:31:11 -0300

    Initial commit

---
+++ src/main/java/br/com/localizador/LocalizadorAgenciasApplication.java
@@ -0,0 +1,13 @@
+package br.com.localizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizadorAgenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizadorAgenciasApplication.class, args);
+	}
+
+}
