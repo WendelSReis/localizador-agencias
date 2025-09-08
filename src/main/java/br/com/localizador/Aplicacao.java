31758c1df25f820ca6b62a7d59045564dfadcc12
WendelSReis <wendel.m.w18@gmail.com>
2025-09-08 09:46:29 -0300

    chore: configurar H2 em memória, JPA e Actuator

--- src/main/java/br/com/localizador/Aplicacao.java
+++ src/main/java/br/com/localizador/Aplicacao.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocalizadorAgenciasApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LocalizadorAgenciasApplication.class, args);
-	}
-
+public class Aplicacao {
+    public static void main(String[] args) {
+        SpringApplication.run(Aplicacao.class, args);
+    }
 }
